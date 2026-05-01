c7c76b2ce0348bd04644505684fc8b7efaf0071a
kotorin123 <piyo_0204_8953@yahoo.co.jp>
2026-05-01 18:51:55 +0900

    変数名及びメソッド名の修正

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -5,26 +5,26 @@ COLUMN_COUNT = 3
 
 def main
   dirlist = Dir.glob('*')
-  padding_list = padding(dirlist)
-  column_groups = split_into_columns(padding_list)
+  padded_filenames = pad_entries(dirlist)
+  column_groups = split_into_columns(padded_filenames)
 
   column_groups.transpose.each do |row|
     puts row.join
   end
 end
 
-def padding(dirlist)
+def pad_entries(dirlist)
   max_length = dirlist.map(&:length).max
   dirlist.map do |entry|
     entry.ljust(max_length + 5)
   end
 end
 
-def split_into_columns(padding_list)
-  rows_per_column = padding_list.size.ceildiv(COLUMN_COUNT)
-  return padding_list if rows_per_column.zero?
+def split_into_columns(padded_filenames)
+  rows_per_column = padded_filenames.size.ceildiv(COLUMN_COUNT)
+  return padded_filenames if rows_per_column.zero?
 
-  column_groups = padding_list.each_slice(rows_per_column).to_a
+  column_groups = padded_filenames.each_slice(rows_per_column).to_a
 
   column_groups.each do |column|
     next if column.size == rows_per_column
--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -2,31 +2,25 @@ require 'minitest/autorun'
 require_relative '../lib/ls'
 
 class LsTest < Minitest::Test
-  def test_padding
+  def test_pad_entries
     dirlist = ["01.txt", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
     expected = ["01.txt     ", "02         ", "03         ", "04         ", "05         ", "06         ", "07         ", "08         ", "09         ", "10         "]
 
-    assert_equal expected, padding(dirlist)
+    assert_equal expected, pad_entries(dirlist)
   end
 
   def test_split_into_columns1
-    padding_list = ["01.txt     ", "02         ", "03         ", "04         ", "05         ", "06         "]
+    padded_filenames = ["01.txt     ", "02         ", "03         ", "04         ", "05         ", "06         "]
     expected = [["01.txt     ", "02         "], ["03         ", "04         "], ["05         ", "06         "]]
 
-    assert_equal expected, split_into_columns(padding_list)
+    assert_equal expected, split_into_columns(padded_filenames)
   end
 
   def test_split_into_columns2
-    padding_list = ["01.txt     ", "02         ", "03         ", "04         ", "05         ", "06         ", "07         ", "08         ", "09         ", "10         "]
+    padded_filenames = ["01.txt     ", "02         ", "03         ", "04         ", "05         ", "06         ", "07         ", "08         ", "09         ", "10         "]
     expected = [["01.txt     ", "02         ", "03         ", "04         "], ["05         ", "06         ", "07         ", "08         "], ["09         ", "10         ",nil,nil]]
     
-    assert_equal expected, split_into_columns(padding_list)
+    assert_equal expected, split_into_columns(padded_filenames)
   end
 
-  def test_output_format
-    column_groups = [["01.txt     ", "02         ", "03         ", "04         "], ["05         ", "06         ", "07         ", "08         "], ["09         ", "10         ",nil,nil]]
-    expected = [["01.txt     ", "05         ", "09         "], ["02         ", "06         ", "10         "], ["03         ", "07         ", nil], ["04         ", "08         ", nil]]
-    
-    assert_equal expected, output_format(column_groups)
-  end
 end
